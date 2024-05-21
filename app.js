de043b5b69f71b5099232f521578479e0cfa9912
aowjarkwk <justgam12@gmail.com>
2024-05-21 20:35:41 +0900

    상품 등록, 상품 목록 조회

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import { DATABASE_URL } from "./env.js";
+import Product from "./models/product.js";
 
 mongoose.connect(DATABASE_URL).then(() => console.log("Connected to DB"));
 const app = express();
@@ -22,3 +23,36 @@ const asyncHandler = (handler) => {
     }
   };
 };
+
+app.get(
+  "/products",
+  asyncHandler(async (req, res) => {
+    /**
+     * 쿼리 파라미터
+     * - page : 페이지 번호
+     * - pageSize : 페이지 당 상품 수
+     * - orderBy : 정렬 기준 favorite, recent (기본값: recent)
+     * - keyword : 검색 키워드
+     */
+    const orderBy = req.query.sort;
+    const count = Number(req.query.count) || 0;
+
+    const sortOption = orderBy === "favorite" ? { favoriteCount: "desc" } : { createdAt: "desc" };
+
+    const product = await Product.find().sort(sortOption).limit(count);
+
+    res.send(product);
+  })
+);
+
+app.post(
+  "/products",
+  asyncHandler(async (req, res) => {
+    const newProduct = await Product.create(req.body);
+    res.status(201).send(newProduct);
+  })
+);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
--- data/mock.js
+++ data/mock.js
@@ -2,7 +2,7 @@ const data = [
   {
     favoriteCount: 0,
     ownerId: 1,
-    images: ["https://example.com/..."],
+    images: ["https://sitem.ssgcdn.com/62/11/49/item/1000559491162_i1_1100.jpg"],
     tags: ["판다인형", "인형", "판다"],
     price: 700000,
     description: "판다인형 판다",
@@ -11,7 +11,9 @@ const data = [
   {
     favoriteCount: 2,
     ownerId: 2,
-    images: ["https://example.com/..."],
+    images: [
+      "https://view01.wemep.co.kr/wmp-product/4/879/2515748794/pm_ebifv5nrjsyf.jpg?1683280710&f=webp&w=460&h=460",
+    ],
     tags: ["판다인형", "인형", "판다"],
     price: 7000,
     description: "판다인형 안판다",
