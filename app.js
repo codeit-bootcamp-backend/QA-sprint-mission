8cf56a025ce50679307238ccb899626d21bc7a7a
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-05-23 15:57:25 +0900

    feat : 이미지 처리 기능 추가

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import userRoutes from "./module/users/user.controller.js";
 import boardRoutes from "./module/board/board.controller.js";
 import commentRoutes from "./module/comment/comment.controller.js";
 import cors from "cors";
+import imageUploadRoutes from "./module/imageUpload/ImageUpload.controller.js";
+
 dotenv.config();
 
 const corsOptions = {
@@ -19,5 +21,6 @@ app.use("/products", productRoutes);
 app.use("/users", userRoutes);
 app.use("/boards", boardRoutes);
 app.use("/comments", commentRoutes);
+app.use("/upload", imageUploadRoutes);
 
 app.listen(process.env.PORT || 3000, () => console.log("Server Started"));
