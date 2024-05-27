6d4ea8ef03b3afbd2d8392d21a5bc98c12b6f594
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-05-27 20:29:51 +0900

    docs: 계속 작업중

--- src/module/products/products.controller.ts
+++ src/module/products/products.controller.ts
@@ -17,9 +17,58 @@ const productRoutes = Router();
 /**
  * @openapi
  * '/products':
+ *   post:
+ *     tags:
+ *     - products
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               images:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                 example:
+ *                   - "https://example.com/..."
+ *               tags:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                 example:
+ *                   - "전자제품"
+ *               price:
+ *                 type: integer
+ *                 example: 0
+ *               description:
+ *                 type: string
+ *                 example: "string"
+ *               name:
+ *                 type: string
+ *                 example: "상품 이름"
+ *     responses:
+ *       200:
+ *         description: Ok
+ *         content:
+ *           application/json:
+ *             example:
+ *               id: 1
+ *               name: "상품 이름"
+ *               description: "string"
+ *               price: 0
+ *               tags:
+ *               - "전자제품"
+ *               images:
+ *               - "https://example.com/..."
+ *               ownerId: 1
+ *               favoriteCount: 0
+ *               createdAt: "2024-05-27T10:13:03.625Z"
  *   get:
  *     tags:
  *     - products
+ *     description: 상품 목록 조회
  *     parameters:
  *       - in: query
  *         name: page
@@ -72,6 +121,11 @@ productRoutes
 	.route('/')
 	.get(asyncHandler(getProductList))
 	.post(asyncHandler(createProduct));
+
+/**
+ * @openapi
+ * '/products':
+ */
 
 productRoutes
 	.route('/:id')
