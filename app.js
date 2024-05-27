a758889835369a4165a80381e09c81f97d9d8711
aowjarkwk <justgam12@gmail.com>
2024-05-27 12:42:48 +0900

    [#M10] feat : 이미지 업로드 기능 구현

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 import express from "express";
+import multer from "multer";
+import path from "path";
 import { assert } from "superstruct";
 import { CreateArticle, CreateComment, CreateProduct, PatchArticle, PatchComment, PatchProduct } from "./structs.js";
+const SERVER_URL = "http://localhost:3000";
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/images/upload", upload.single("image"), async (req, res) => {
+  const file = req.file;
+  console.log(req);
+  if (!file) {
+    return res.status(400).send("이미지 파일을 선택해주세요.");
+  }
+  const imagePath = file.path;
+  const imageUrl = `${SERVER_URL}/${imagePath.replace(/\\/g, "/")}`;
+  const image = await prisma.image.create({
+    data: {
+      imagePath: imagePath,
+    },
+  });
+
+  res.status(200).json({ url: imageUrl });
+});
 
 const asyncHandler = (handler) => {
   return async (req, res) => {
