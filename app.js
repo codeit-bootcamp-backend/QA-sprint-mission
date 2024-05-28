00ab5514915ea42b1fa235ee9aa9692314eddbc8
aowjarkwk <justgam12@gmail.com>
2024-05-28 11:49:23 +0900

    [#M10] feat : morgan을 사용하여 모든 요청의 상세 정보를 로그로 기록

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 import express from "express";
+import fs from "fs";
+import moment from "moment-timezone";
+import morgan from "morgan";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 import articlesRouter from "./routes/articles.js";
 import authRouter from "./routes/auth.js";
 import imagesRouter from "./routes/images.js";
 import productsRouter from "./routes/products.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
+
+morgan.token("date", (req, res, tz) => {
+  return moment().tz(tz).format("YYYY-MM-DD HH:mm:ss");
+});
+// 커스텀 포맷 설정: 한국 시간대의 현재 시간을 포함
+const customFormat = ":method :url :status :res[content-length] - :response-time ms - :date[Asia/Seoul]";
+
+// 로그 파일 스트림 생성
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" });
+
+// morgan 미들웨어 설정 (로그 파일에 기록)
+app.use(morgan(customFormat, { stream: accessLogStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
